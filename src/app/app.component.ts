96808db9afe937ef0876aa3662d65d8b5c10927d
Jorge Guerrero <jorgegro1999@gmail.com>
2018-07-31 20:03:48 -0500

    Updating portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WOW } from 'wowjs/dist/wow.min';
 
 
 
@@ -9,7 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  mouseEnter(a: string){
-    a
-  }
+  
+  
+  educationInfo =[
+    {
+      title: "High School Diploma",
+      description: "Graduated from Early College High School as part of the top 10% of my class, consisting of almost 300 students. While in high school I was part of a program called GearUp. This program help me to grow as a person polishing many of my people skils and leadership habilities."
+    },
+    {
+      title: "South Texas College Graduated",
+      description: "During my high school, I had many dual credit classes that enabled me to progress further into my post-secondary education. Now I'm a graduate at the age of 18 years-old."
+    },
+    {
+      title: "Computer Engineer at UTRGV",
+      description: "My interest in computer and coding goes far beyond web developement. I want to create, build and implement my own ideas. Innovate the world in my own way. As a sophomore looking to finish and complete my arsenal for my dreams."
+    }
+  ]
 }
