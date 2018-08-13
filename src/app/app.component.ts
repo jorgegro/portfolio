993e57913dce250f57cfc793ef448f7b7ca08a04
Jorge Guerrero <jorgegro1999@gmail.com>
2018-08-13 14:25:45 -0500

    Update scroll animation and addition of PokeDisplay project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,5 +32,33 @@ export class AppComponent {
   techSkills = ["Angular", "HTML5", "Express", "CSS3", "Bootstrap 4", "SQL", "Wordpress", "RESTful APIs", "C++", "Javascript", "Sass",
 ];
 
+  projects = [
+    {
+      title: "Name Tag Id Generator App",
+      description: "Created use pure HTML5, CSS3 and Javascript 5 in this web application the user can input into the id fields to fill out their id. DOM it would get the information by their id and populate the id with those values.",
+      link: "https://jorgegro.github.io/idcardapp/",
+      pic: "/assets/Id-generator.png"
+    },
+    {
+      title: "To Do App",
+      description: "Developed in Angular 6. In this ToDo App has basic CRUD (create, read, update, delete) functions. With the help of bootstrap utilizing the row and col grid system, googleFonts and fontawesome libraries, utilizing angular CLI and finally unsplash to add images and color to the web app.",
+      link: "https://github.com/jorgegro/todoApp",
+      pic: "/assets/Todo-app.png"
+    },
+    {
+      title: "Zombie Quiz App",
+      description: "Build with Angular 6, utilizing routing, services and JavaScript logic. In this app the user gets to answer a quiz related whether if they could survive a zombie apocalypse, as users select answer the JavaScript logic process their results.",
+      link: "https://jorgegro.github.io/zombie-apocalypse-quiz/",
+      pic: "/assets/zombie-quiz.png"
+    },
+    {
+      title: "PokeDisplay",
+      description: `Using Angular 5+, RESTful API and Bootstrap 4 the PokeDisplay is a web application where users may look for their favorite pokémons. Once they looked for their pokemon utilizing the PokeApi a get request seek for the pokemon's name, sprite, and nature.
+      Users are able to add this pokémon into their "team" in which they can collect a limit of 6 pokémon similar to the games.`,
+      link: "https://github.com/jorgegro/MyPokeTeam",
+      pic: "/assets/PokeDisplay.png"
+    }
+  ]
+
 
 }
